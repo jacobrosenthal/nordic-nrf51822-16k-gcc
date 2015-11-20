ce3e08ba6eb6490b969616c5eac95839bb6db170
Jacob Rosenthal <jakerosenthal@gmail.com>
2015-11-19 23:47:56 -0500

    merge 0.1.3

--- CMake/toolchain.cmake
+++ CMake/toolchain.cmake
@@ -51,10 +51,12 @@ set(CMAKE_MODULE_LINKER_FLAGS_INIT "${CMAKE_MODULE_LINKER_FLAGS_INIT} -mcpu=cort
 set(CMAKE_EXE_LINKER_FLAGS_INIT    "${CMAKE_EXE_LINKER_FLAGS_INIT} -mcpu=cortex-m0 -mthumb -T\"${NRF51822_LINKER_FLAGS_FILE_PATH}\"")
 
 # used by the apply_target_rules function below:
-set(NRF51822_SOFTDEVICE_HEX_FILE "${NRF51822_SOFTDEVICE_FILE_PATH}")
-set(NRF51822_MERGE_HEX_SCRIPT    "${CMAKE_CURRENT_LIST_DIR}/../scripts/merge_hex.py")
-set(NRF51822_GEN_DAT_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../scripts/generate_dat.py")
-set(NRF51822_BOOTLOADER_HEX_FILE "${CMAKE_CURRENT_LIST_DIR}/../../../yotta_modules/ble-nrf51822/bootloader/s130_nrf51_1.0.0_bootloader.hex")
+set(NRF51822_SOFTDEVICE_HEX_FILE    "${NRF51822_SOFTDEVICE_FILE_PATH}")
+set(NRF51822_MERGE_HEX_SCRIPT       "${CMAKE_CURRENT_LIST_DIR}/../scripts/merge_hex.py")
+set(NRF51822_GEN_DAT_SCRIPT         "${CMAKE_CURRENT_LIST_DIR}/../scripts/generate_dat.py")
+set(NRF51822_BOOTLOADER_HEX_FILE    "${CMAKE_CURRENT_LIST_DIR}/../../../yotta_modules/ble-nrf51822/bootloader/s130_nrf51_1.0.0_bootloader.hex")
+set(NRF51822_MEMORY_INFO_SCRIPT     "${CMAKE_CURRENT_LIST_DIR}/../scripts/memory_info.py")
+set(NRF51822_HEAP_WARNING_THRESHOLD 1024)
 
 # define a function for yotta to apply target-specific rules to build products,
 # in our case we need to convert the built elf file to .hex, and add the
@@ -75,6 +77,13 @@ function(yotta_apply_target_rules target_type target_name)
             COMMENT "generating .dat"
             COMMAND srec_cat ${NRF51822_SOFTDEVICE_HEX_FILE} -intel ${target_name}.hex -intel ${NRF51822_BOOTLOADER_HEX_FILE} -intel -exclude 0x3FC00 0x3FC20 -generate 0x3FC00 0x3FC04 -l-e-constant 0x01 4 -generate 0x3FC04 0x3FC08 -l-e-constant 0x00 4 -generate 0x3FC08 0x3FC0C -l-e-constant 0xFE 4 -generate 0x3FC0C 0x3FC20 -constant 0x00 -o ${target_name}-combined-bootloader.hex -intel
             COMMENT "building fota ${target_name}-combined-bootloader.hex"
+            VERBATIM
+        )
+        add_custom_command(TARGET ${target_name}
+            POST_BUILD
+            # printing memory usage information
+            COMMAND python ${NRF51822_MEMORY_INFO_SCRIPT} ${target_name} ${NRF51822_HEAP_WARNING_THRESHOLD}
+            COMMENT "printing memory usage information"
             VERBATIM
         )
     endif()
